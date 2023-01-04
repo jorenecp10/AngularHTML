029b28ef2b402dcaa140b4bcf99562e70cd06627
AgendaZuana <jorenecp10@gmail.com>
2023-01-03 20:44:13 -0500

    pagina about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ItemComponent
+    ItemComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
